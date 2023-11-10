0e6786f24e2b72de3e7fa789323098bc852cbe5c
Yusan1234 <38897520+Yusan1234@users.noreply.github.com>
2023-11-10 02:33:16 -0500

    Update index.ts

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis";
 
-export const redisClient = new Redis("redis://default:cd83224232644d4792e117d766e4094d@us1-emerging-zebra-38052.upstash.io:38052");
+export const redisClient = new Redis("");
